fe1d56352b03eb973e3a74b1e03f2bce1c454218
SrihariguptaK <sriharigk@nousinfo.com>
2026-01-14 23:11:05 +0530

    Add test automation: story-6.spec.ts

---
+++ AutomationScript/e9679cb1-e575-4343-834c-235c30aa93a0/epic-1/feature-2/story-6.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('As Scheduler, I want to view employee schedules in multiple formats to facilitate planning', async ({ page }) => {
+  // TODO: Implement test steps for: As Scheduler, I want to view employee schedules in multiple formats to facilitate planning
+  // Response parsing failed - please review and implement manually
+  await page.goto('/');
+  await expect(page).toHaveTitle(/.*/)
+});
