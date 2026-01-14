bcae61a96c91bf1d0c9094e4735f705164be191a
SrihariguptaK <sriharigk@nousinfo.com>
2026-01-14 23:03:44 +0530

    Add test automation: story-6.spec.ts

--- AutomationScript/e9679cb1-e575-4343-834c-235c30aa93a0/epic-1/feature-3/story-6.spec.ts
+++ AutomationScript/e9679cb1-e575-4343-834c-235c30aa93a0/epic-1/feature-3/story-6.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('As Scheduling Manager, I want to manually adjust employee schedules to handle exceptions and changes', async ({ page }) => {
-  // TODO: Implement test steps for: As Scheduling Manager, I want to manually adjust employee schedules to handle exceptions and changes
+test('As HR Manager, I want to generate reports on schedule adherence to monitor compliance and coverage', async ({ page }) => {
+  // TODO: Implement test steps for: As HR Manager, I want to generate reports on schedule adherence to monitor compliance and coverage
   // Response parsing failed - please review and implement manually
   await page.goto('/');
   await expect(page).toHaveTitle(/.*/)
