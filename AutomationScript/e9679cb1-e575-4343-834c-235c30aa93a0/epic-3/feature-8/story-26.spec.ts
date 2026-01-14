df080a0e5ead190660bf54c1ccafb9a82adbc16f
SrihariguptaK <sriharigk@nousinfo.com>
2026-01-14 23:11:25 +0530

    Add test automation: story-26.spec.ts

---
+++ AutomationScript/e9679cb1-e575-4343-834c-235c30aa93a0/epic-3/feature-8/story-26.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('As Scheduler, I want to submit schedule change requests to achieve controlled modification of schedules', async ({ page }) => {
+  // TODO: Implement test steps for: As Scheduler, I want to submit schedule change requests to achieve controlled modification of schedules
+  // Response parsing failed - please review and implement manually
+  await page.goto('/');
+  await expect(page).toHaveTitle(/.*/)
+});
