ae1b6ef74d34c2f0e04751e6b65a4ae8f54c8a57
SrihariguptaK <sriharigk@nousinfo.com>
2026-01-14 23:04:25 +0530

    Add test automation: story-21.spec.ts

--- AutomationScript/e9679cb1-e575-4343-834c-235c30aa93a0/epic-3/feature-7/story-21.spec.ts
+++ AutomationScript/e9679cb1-e575-4343-834c-235c30aa93a0/epic-3/feature-7/story-21.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('As Employee, I want to submit schedule change requests to achieve timely updates to my work schedule', async ({ page }) => {
-  // TODO: Implement test steps for: As Employee, I want to submit schedule change requests to achieve timely updates to my work schedule
+test('As Administrator, I want to create approval workflows to achieve tailored schedule change approvals', async ({ page }) => {
+  // TODO: Implement test steps for: As Administrator, I want to create approval workflows to achieve tailored schedule change approvals
   // Response parsing failed - please review and implement manually
   await page.goto('/');
   await expect(page).toHaveTitle(/.*/)
