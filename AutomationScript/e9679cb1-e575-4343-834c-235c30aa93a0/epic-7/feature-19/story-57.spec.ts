bbf57bc96a190b5a94c629081fbf5e6e3d5b809e
SrihariguptaK <sriharigk@nousinfo.com>
2026-01-14 23:13:45 +0530

    Add test automation: story-57.spec.ts

---
+++ AutomationScript/e9679cb1-e575-4343-834c-235c30aa93a0/epic-7/feature-19/story-57.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('As Manager, I want to create tasks with detailed descriptions to achieve clear communication of work requirements', async ({ page }) => {
+  // TODO: Implement test steps for: As Manager, I want to create tasks with detailed descriptions to achieve clear communication of work requirements
+  // Response parsing failed - please review and implement manually
+  await page.goto('/');
+  await expect(page).toHaveTitle(/.*/)
+});
