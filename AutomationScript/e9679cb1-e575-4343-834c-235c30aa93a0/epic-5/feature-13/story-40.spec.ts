df77c9a6a265b20b7ea24734e581650facbc8a9a
SrihariguptaK <sriharigk@nousinfo.com>
2026-01-14 23:12:17 +0530

    Add test automation: story-40.spec.ts

---
+++ AutomationScript/e9679cb1-e575-4343-834c-235c30aa93a0/epic-5/feature-13/story-40.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('As Biometric System Administrator, I want to configure biometric devices to achieve seamless attendance data capture', async ({ page }) => {
+  // TODO: Implement test steps for: As Biometric System Administrator, I want to configure biometric devices to achieve seamless attendance data capture
+  // Response parsing failed - please review and implement manually
+  await page.goto('/');
+  await expect(page).toHaveTitle(/.*/)
+});
