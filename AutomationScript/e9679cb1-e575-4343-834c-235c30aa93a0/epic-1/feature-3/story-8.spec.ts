d2ede26f79e615b4f407c7993d1cdb9302105531
SrihariguptaK <sriharigk@nousinfo.com>
2026-01-14 23:10:17 +0530

    Add test automation: story-8.spec.ts

---
+++ AutomationScript/e9679cb1-e575-4343-834c-235c30aa93a0/epic-1/feature-3/story-8.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('As Scheduler, I want to receive notifications about schedule assignments and changes to stay informed', async ({ page }) => {
+  // TODO: Implement test steps for: As Scheduler, I want to receive notifications about schedule assignments and changes to stay informed
+  // Response parsing failed - please review and implement manually
+  await page.goto('/');
+  await expect(page).toHaveTitle(/.*/)
+});
