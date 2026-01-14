4137c4b6629adca57d8b500cbb605c8dff1a5bb4
SrihariguptaK <sriharigk@nousinfo.com>
2026-01-14 23:03:42 +0530

    Add test automation: story-1.spec.ts

--- AutomationScript/e9679cb1-e575-4343-834c-235c30aa93a0/epic-1/feature-1/story-1.spec.ts
+++ AutomationScript/e9679cb1-e575-4343-834c-235c30aa93a0/epic-1/feature-1/story-1.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('As Scheduling Manager, I want to create shift templates to achieve standardized shift definitions', async ({ page }) => {
-  // TODO: Implement test steps for: As Scheduling Manager, I want to create shift templates to achieve standardized shift definitions
+test('As HR Manager, I want to create shift templates to achieve standardized shift definitions', async ({ page }) => {
+  // TODO: Implement test steps for: As HR Manager, I want to create shift templates to achieve standardized shift definitions
   // Response parsing failed - please review and implement manually
   await page.goto('/');
   await expect(page).toHaveTitle(/.*/)
