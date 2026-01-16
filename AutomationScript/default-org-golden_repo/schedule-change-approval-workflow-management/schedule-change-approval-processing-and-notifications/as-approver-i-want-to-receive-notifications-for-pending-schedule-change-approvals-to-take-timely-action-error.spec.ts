af3539ee886b627ab4d91b06439301c35866868d
SrihariguptaK <sriharigk@nousinfo.com>
2026-01-16 18:43:19 +0530

    Delete AutomationScript/default-org-golden_repo directory

--- AutomationScript/default-org-golden_repo/schedule-change-approval-workflow-management/schedule-change-approval-processing-and-notifications/as-approver-i-want-to-receive-notifications-for-pending-schedule-change-approvals-to-take-timely-action-error.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-// ERROR: Failed to generate test for this story
-// Story: As Approver, I want to receive notifications for pending schedule change approvals to take timely action
-// Error: Failed to generate Playwright scripts: This operation was aborted
-
-// This is a placeholder - manual test creation needed
